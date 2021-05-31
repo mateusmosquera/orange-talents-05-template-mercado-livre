4d2af8b0363e753a5c7ca5884a9d46ea0e9e96bb
mateusmosquera <mateusmosquera@hotmail.com>
2021-05-31 15:50:55 -0300

    setup

---
+++ src/test/java/br/com/zupacademy/mateus/desafiomercadolivre/DesafioMercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.mateus.desafiomercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
